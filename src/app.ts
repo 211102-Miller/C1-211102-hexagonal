708adf922fc20aed9601158cfdb9373f98fef3c9
211102-Miller <84791228+211102-Miller@users.noreply.github.com>
2023-09-29 02:01:31 -0600

    todoo

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import { Signale } from 'signale';
+import { userRoutes } from './users/insfraestructure/userRoutes';
+import { connectToPostgreSQL } from './database/postgresql';
+import { bookRoutes } from './books/infraestructure/bookRutes';
+import { reviewRoutes } from './rewies/insfraestructure/reviewRoutes';
+
+const app = express();
+const signale = new Signale();
+const port = 3000;
+
+app.use(express.json());
+app.use("/user",userRoutes)
+app.use("/book",bookRoutes)
+app.use("/review",reviewRoutes)
+
+
+//connectToPostgreSQL();
+
+app.listen(port, () => {
+  signale.success(`Servidor escuchando en el puerto ${port}`)
+});
