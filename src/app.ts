26d7eb1a112f07a70d3ce5723244886ff4807a1b
nombre <211102@ids.upchiapas.edu.mx>
2023-09-30 10:54:14 -0600

    Book terminado solo falta token

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { Signale } from 'signale';
 import { connectToPostgreSQL } from './database/postgresql';
 import { bookRoutes } from './books/infraestructure/bookRutes';
-import {leadRoutes}from "./books/infraestructure/routesLead"
+
 
 const app = express();
 const signale = new Signale();
@@ -10,7 +10,6 @@ const port = 3000;
 
 app.use(express.json());
 app.use("/book",bookRoutes)
-app.use("/lead",leadRoutes)
 
 
 
